5a31970dea0a868705ab69569cfd00387d415829
n22-webb <nicola5.webb@live.uwe.ac.uk>
2022-05-19 14:47:39 +0100

    Web update

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, request, url_for, redirect, render_template
+from flask import Flask, request, url_for, redirect, render_template, session
 
 app = Flask(__name__)
+app.secret_key = 'EHEHEHE'
+app.config['SESSION_COOKIE_SAMESITE'] = "None"
 
-
-@app.route('/')
+@app.route("/", methods=['GET','POST'])
 def index():
+    if request.method == 'POST':
+        return redirect(url_for('consent_page'))
     return render_template('index.html')
+
 
 @app.route('/help')
 def help_page():
@@ -13,7 +17,14 @@ def help_page():
 
 @app.route('/consent')
 def consent_page():
+    # session['name'] = request.form['name']
+    data = []
+    if 'name' in session:
+        data = session['name']
+        print(data)
+        return render_template('consent.html', data=data)
     return render_template('consent.html')
+
 
 @app.route('/noconsent')
 def noconsent_page():
@@ -41,8 +52,21 @@ def how_page():
 @app.route('/missions', methods=['GET', 'POST'])
 def missions_page():
     if request.method == 'POST':
-        print(request.get_json())  # parse as JSON
-        return redirect(url_for('about_page'))
+        content = request.json.get('game')
+        print(content)
+        if content == 'over':
+            timer_over = True   
+
+            return redirect(url_for('thankyou'))
         # return 'Sucesss', 200
 
     return render_template('missions.html')
+
+@app.route('/thankyou')
+def thankyou_page():
+    return render_template('thankyou.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0")
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,48 @@
-startTimer();
-$('#timer').text(10 + ":" + 0);
+$(document).ready(function(){
+  startTimer();
+  $('#timer').text(10 + ":" + 0);
+  
+
+  $(".indv-mission-yes-btn1").click(function(){
+    passedIndvMission(1);
+  });
+
+  $(".indv-mission-yes-btn2").click(function(){
+    passedIndvMission(2);
+  });
+
+  $(".group-mission-yes-btn1").click(function(){
+    passedGroupMission(1);
+  });
+
+  $(".group-mission-yes-btn2").click(function(){
+    passedGroupMission(2);
+  });
+
+  $(".indv-mission-no-btn1").click(function(){
+    skipIndvMission(1);
+  });
+
+  $(".indv-mission-no-btn2").click(function(){
+    skipIndvMission(2);
+  });
+
+  $(".group-mission-no-btn1").click(function(){
+    skipGroupMission(1);
+  });
+
+  $(".group-mission-no-btn2").click(function(){
+    skipGroupMission(2);
+  });
+
+  // $(".name-button").click(function(){
+  //   alert("df");
+  // });
+ 
+});
+
+
+
 
 
 function startTimer() {
@@ -17,7 +60,6 @@ function startTimer() {
   setTimeout(startTimer, 1); //1000
   onEnd(presentTime);
 }
- 
 
 function checkSecond(sec) {
   if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
@@ -28,7 +70,8 @@ function checkSecond(sec) {
 
 function onEnd(timerText){
    if(timerText == "0:01"){
-    const data = {'name': 'Tom'};
+    $('#timesupmodal').modal('show');
+    const data = {'game': 'over'};
     fetch('/missions', {
         method: 'POST',
         headers: {
@@ -43,5 +86,63 @@ function onEnd(timerText){
     .catch((error) => {
       console.error('Error:', error);
     });  
+  }
+}
+
+var items = Array("Mission 1", "Mission 2", "Mission 3", "Mission 4");
+
+function passedIndvMission(cardID){
+  var item = items[Math.floor(Math.random()*items.length)];
+  new_text = item
+
+  if(cardID==1){
+    $('#indv-card-text1').text(new_text);
+
+  }
+  else if(cardID==2){
+    $('#indv-card-text2').text(new_text);
+
+  }
+}
+
+function passedGroupMission(cardID){
+  var item = items[Math.floor(Math.random()*items.length)];
+  new_text = item
+
+  if(cardID==1){
+    $('#group-card-text1').text(new_text);
+
+  }
+  else if(cardID==2){
+    $('#group-card-text2').text(new_text);
+
+  }
+}
+
+function skipIndvMission(cardID){
+  var item = items[Math.floor(Math.random()*items.length)];
+  new_text = item
+
+  if(cardID==1){
+    $('#indv-card-text1').text(new_text);
+
+  }
+  else if(cardID==2){
+    $('#indv-card-text2').text(new_text);
+
+  }
+}
+
+function skipGroupMission(cardID){
+  var item = items[Math.floor(Math.random()*items.length)];
+  new_text = item
+
+  if(cardID==1){
+    $('#group-card-text1').text(new_text);
+
+  }
+  else if(cardID==2){
+    $('#group-card-text2').text(new_text);
+
   }
 }
